9721d0b3b6ac24493c0bc568566b502b7886ed6e
Hélcio Humberto <helciohumberto@gmail.com>
2024-02-28 16:34:46 -0300

    ex7_2

--- capitulo07/js/ex7_1.js
+++ capitulo07/js/ex7_1.js
@@ -1,9 +1,20 @@
+// const anuncio = prompt("Anúncio: ")
+// let numPalavras = 0
+// const tam = anuncio.length
+// for(let i = 0; i < tam; i++){
+//   if(anuncio.charAt(i) == " "){
+//     numPalavras++
+//   }
+// }
+// alert(`Anúncio: ${anuncio}\nN. Palavras: ${(numPalavras + 1)}`)
+
 const anuncio = prompt("Anúncio: ")
+
 let numPalavras = 0
-const tam = anuncio.length
-for(let i = 0; i < tam; i++){
-  if(anuncio.charAt(i) == " "){
+for(const letra of anuncio){
+  if(letra == " "){
     numPalavras++
   }
 }
+
 alert(`Anúncio: ${anuncio}\nN. Palavras: ${(numPalavras + 1)}`)
---
+++ capitulo07/js/ex7_2.js
@@ -0,0 +1,19 @@
+const form = document.querySelector("form")
+const resp = document.querySelector("span")
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault()
+  const fruta = form.inFruta.value.toUpperCase()
+  let resposta = ""
+
+  for(letra of fruta){
+    if(letra == fruta.charAt(0)){
+      resposta += fruta.charAt(0)
+    }else{
+      resposta += "_"
+    }
+  }
+
+  resp.innerText = resposta
+  form.inFruta.value = "*".repeat(fruta.length)
+})
